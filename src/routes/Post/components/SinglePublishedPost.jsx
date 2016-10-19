a4a2a9d39794e8dd7ff82302746c4ca94e51d926
Shawn Zhu <shawn.zhu@level-studios.com>
2016-10-19 13:13:29 -0700

    enable fetching single post with wordpress rest api

--- src/routes/Post/components/SinglePublishedPost.jsx
+++ src/routes/Post/components/SinglePublishedPost.jsx
@@ -33,7 +33,7 @@ class PopulatedPost extends Base {
     console.log('Single post componentWillMount *****');
     const { item, routeParams, fetchPosts } = this.props
     if (item === null) {
-      const api = APIS.postSingle(routeParams.slug)
+      const api = APIS.postSingle(routeParams.slug, 'es')
       console.log(`fetching post from ${api}`)
       fetchPosts(api)
     }
--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,4 +1,4 @@
 export default {
   postList: (limit=100) => `http://localhost:8888/wp-json/wp/v2/posts?per_page=${limit}`,
-  postSingle: (slug) => `http://localhost:8888/wp-json/wp/v2/posts?slug=${slug}`
+  postSingle: (slug, language='en') => `http://localhost:8888/${language}/wp-json/wp/v2/posts?slug=${slug}`
 }
