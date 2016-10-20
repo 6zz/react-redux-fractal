c2b3f8ca75e6e32c6c4004f5f94576b4bbce909e
Shawn Zhu <shawn.zhu@level-studios.com>
2016-10-20 15:44:50 -0700

    fix header for demo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,10 @@ import './Header.scss'
 
 export const Header = () => (
   <div>
-    <h1>React Redux Starter Kit</h1>
+    <h1>Level Studio Wordpress SPA Demo</h1>
     <IndexLink to='/' activeClassName='route--active'>
       Home
     </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='route--active'>
-      Counter
-    </Link>
     {' · '}
     <Link to='/stories' activeClassName='route--active'>
       Stories
