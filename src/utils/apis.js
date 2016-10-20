83f0b27abdfe6db5e750e41d3e6b218bcbafd1cf
Shawn Zhu <shawn.zhu@level-studios.com>
2016-10-20 12:00:58 -0700

    fix wordpress rest api path to support production mode and development mode

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,4 +1,6 @@
+const WP_REST_URI = window.WP_REST_SERVER || ''
+
 export default {
-  postList: (limit=100) => `http://localhost:8888/wp-json/wp/v2/posts?per_page=${limit}`,
-  postSingle: (slug, language='en') => `http://localhost:8888/${language}/wp-json/wp/v2/posts?slug=${slug}`
+  postList: (limit=100) => `${WP_REST_URI}/wp-json/wp/v2/posts?per_page=${limit}`,
+  postSingle: (slug, language='en') => `${WP_REST_URI}/${language}/wp-json/wp/v2/posts?slug=${slug}`
 }
